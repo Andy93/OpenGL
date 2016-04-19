3b61b057c5d2aee7064a27620eafeb4705cde9d6
Andy93 <oneillandrew101@gmail.com>
2016-04-19 16:50:01 +0100

    Update x1.c
    This is without comments on how to make it work......although most is simplistic in how it works

--- x1.c
+++ x1.c
@@ -1,12 +1,14 @@
 #include <GL/gl.h>
-#include <stdio.h>
 #include <GL/glext.h>
 #include <H:/lab/glut.h>
 
-GLfloat xi,xf,yi,yf;
+GLfloat xa,xb,xc,xd,xe,xf,xg,xh,xi,xj,xk;
+GLfloat ya,yb,yc,yd,ye,yf,yg,yh,yi,yj,yk;
 GLfloat hh;
 GLfloat array[3]; 
-float r,g,b;
+float r;
+float g;
+float b;
 
 int width = 500;
 int height = 500;
@@ -22,7 +24,10 @@ void display(void)
 	glPointSize(3.0);
 	
 	if(colour == 0){
-		glColor4f(0.7,0.7,0.7, transparency);	
+		r=1.0;
+		g=1.0;
+		b=1.0;
+		glColor4f(r,g,b, transparency);	
 	}
 
 	glFlush();
@@ -52,10 +57,10 @@ void clearScreen(){
 void drawRectangle(int x1,int y1,int x2,int y2)
 {
 	glBegin(GL_POLYGON);
-		glVertex2f(xi,yi);
-		glVertex2f(xi,yf);
-		glVertex2f(xf,yf);
-		glVertex2f(xf,yi);
+		glVertex2f(xa,ya);
+		glVertex2f(xa,yb);
+		glVertex2f(xb,yb);
+		glVertex2f(xb,ya);
 	glEnd();
 	
 	glFlush();
@@ -68,30 +73,25 @@ void createRectangle(GLint button, GLint state, GLint x, GLint y)
 		switch(first)
 		{		
 			case 0:
-				xi=x; 
-				yi=hh-y;
+				xa=x; 
+				ya=hh-y;
 				first = 1;
 				break;
 			case 1:
-				xf=x;
-				yf=hh-y;
-				drawRectangle(xi, yi, xf, yf);
+				xb=x;
+				yb=hh-y;
+				drawRectangle(xa, ya, xb, yb);
 				first = 0;
 		}
 	}
 	return;
 }
-//void putColour(float r,float g,float b){
-//	glColor4f(r,g,b,transparency);
-//colour = 1;
-	
-//}
 
 void touchPallette(GLint button, GLint state, GLint x, GLint y){
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
 	{	
-		yi=hh-y;
-		glReadPixels(x,yi,1,1,GL_RGB,GL_FLOAT,array);
+		ya=hh-y;
+		glReadPixels(x,ya,1,1,GL_RGB,GL_FLOAT,array);
 			r = array[0];
 			g = array[1];
 			b = array[2];
@@ -103,8 +103,8 @@ void drawLine(int x1,int y1,int x2,int y2){
 	
 	glLineWidth(3.0); 
 	glBegin(GL_LINES);
-		glVertex2f(xi,yi);
-		glVertex2f(xf,yf);
+		glVertex2f(x1,y1);
+		glVertex2f(x2,y2);
 	glEnd();
 	
 	glFlush();
@@ -114,14 +114,14 @@ void createLine(GLint button, GLint state, GLint x, GLint y){
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
 		switch(first){		
 			case 0:
-				xi=x; 
-				yi=hh-y;
+				xa=x; 
+				ya=hh-y;
 				first = 1;
 				break;
 			case 1:
-				xf=x;
-				yf=hh-y;
-				drawLine(xi, yi, xf, yf);
+				xb=x;
+				yb=hh-y;
+				drawLine(xa, ya, xb, yb);
 				first = 0;
 		}
 	}
@@ -132,7 +132,7 @@ void drawPoint(int x1,int y1){
 	
 	glLineWidth(3.0); 
 	glBegin(GL_POINTS);
-		glVertex2f(xi,yi);
+		glVertex2f(xa,ya);
 	glEnd();
 	
 	glFlush();
@@ -140,15 +140,388 @@ void drawPoint(int x1,int y1){
 
 void createPoint(GLint button, GLint state, GLint x, GLint y){
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
-		xi=x;
-		yi=hh-y;
-		drawPoint(xi, yi);
+		xa=x;
+		ya=hh-y;
+		drawPoint(xa, ya);
 	}
 	return;
 }
 
-void polygon(){
-	//do later maybe triangle, pentagon, hexagon, heptagon, octagon, nonagon, decagon.
+void drawTriangle(int x1, int x2, int x3, int y1, int y2, int y3){
+	glLineWidth(3.0); 
+	glBegin(GL_TRIANGLES);
+		glVertex2f(x1,y1);
+		glVertex2f(x2,y2);
+		glVertex2f(x3,y3);
+	glEnd();
+	
+	glFlush();
+}
+
+void createTriangle(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first =0;
+				drawTriangle(xa,xb,xc,ya,yb,yc);
+				break;
+		}
+	}
+	return;
+}
+
+void createPentagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				drawLine(xe,ye,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
+}
+
+void createHexagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				xf=x;
+				yf=hh-y;
+				first = 6;
+				drawLine(xe,ye,xf,yf);
+				break;
+			case 6:
+				drawLine(xf,yf,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
+}
+
+void createHeptagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				xf=x;
+				yf=hh-y;
+				first = 6;
+				drawLine(xe,ye,xf,yf);
+				break;
+			case 6:
+				xh=x;
+				yh=hh-y;
+				first = 7;
+				drawLine(xf,yf,xh,yh);
+				break;
+			case 7:
+				drawLine(xh,yh,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
+}
+
+void createOctagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				xf=x;
+				yf=hh-y;
+				first = 6;
+				drawLine(xe,ye,xf,yf);
+				break;
+			case 6:
+				xh=x;
+				yh=hh-y;
+				first = 7;
+				drawLine(xf,yf,xh,yh);
+				break;
+			case 7:
+				xi=x;
+				yi=hh-y;
+				first = 8;
+				drawLine(xh,yh,xi,yi);
+				break;	
+			case 8:
+				drawLine(xi,yi,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
+}
+
+void createNonagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				xf=x;
+				yf=hh-y;
+				first = 6;
+				drawLine(xe,ye,xf,yf);
+				break;
+			case 6:
+				xh=x;
+				yh=hh-y;
+				first = 7;
+				drawLine(xf,yf,xh,yh);
+				break;
+			case 7:
+				xi=x;
+				yi=hh-y;
+				first = 8;
+				drawLine(xh,yh,xi,yi);
+				break;
+			case 8:
+				xj=x;
+				yj=hh-y;
+				first = 9;
+				drawLine(xi,yi,xj,yj);
+				break;	
+			case 9:
+				drawLine(xj,yj,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
+}
+
+void createDecagon(GLint button, GLint state, GLint x, GLint y){
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
+		switch(first){	
+		
+			case 0:
+				xa=x; 
+				ya=hh-y;
+				first = 1;
+				break;
+			case 1:
+				xb=x;
+				yb=hh-y;
+				first = 2;
+				drawLine(xa,ya,xb,yb);
+				break;
+			case 2:
+				xc=x;
+				yc=hh-y;
+				first = 3;
+				drawLine(xb,yb,xc,yc);
+				break;
+			case 3:
+				xd=x;
+				yd=hh-y;
+				first = 4;
+				drawLine(xc,yc,xd,yd);
+				break;
+			case 4:
+				xe=x;
+				ye=hh-y;
+				first = 5;
+				drawLine(xd,yd,xe,ye);
+				break;
+			case 5:
+				xf=x;
+				yf=hh-y;
+				first = 6;
+				drawLine(xe,ye,xf,yf);
+				break;
+			case 6:
+				xh=x;
+				yh=hh-y;
+				first = 7;
+				drawLine(xf,yf,xh,yh);
+				break;
+			case 7:
+				xi=x;
+				yi=hh-y;
+				first = 8;
+				drawLine(xh,yh,xi,yi);
+				break;
+			case 8:
+				xj=x;
+				yj=hh-y;
+				first = 9;
+				drawLine(xi,yi,xj,yj);
+				break;
+			case 9:
+				xk=x;
+				yk=hh-y;
+				first = 10;
+				drawLine(xj,yj,xk,yk);
+				break;
+			case 10:
+				drawLine(xk,yk,xa,ya);
+				first = 0;
+				break;
+		}
+	}
+	return;
 }
 
 //menu options for all cases
@@ -182,79 +555,142 @@ void menu(int op) {
 			glutMouseFunc(createRectangle);
 			break;
 		
-		//polygon
+		//Pentagon		
+		case 'r':
+			glutMouseFunc(createPentagon);
+			break;
+			
+		//Hexagon		
+		case 's':
+			glutMouseFunc(createHexagon);
+			break;
+			
+		//Heptagon		
+		case 't':
+			glutMouseFunc(createHeptagon);
+			break;
+			
+		//Octagon
+		case 'u':
+			glutMouseFunc(createOctagon);
+			break;
+		
+		//Nonagon
+		case 'v':
+			glutMouseFunc(createNonagon);
+			break;
+			
+		//Decagon
+		case 'w':
+			glutMouseFunc(createDecagon);
+			break;
+		
+		//triangle
 		case 'd':
-			exit(0);
+			glutMouseFunc(createTriangle);
 			break;
 		
 		//COLOURS - source : http://www.cs.brandeis.edu/~cs155/OpenGL%20Lecture_02.pdf
 		//red
 		case 'e':
-			glColor4f(1.0,0.0,0.0,transparency);
+			r=1.0;
+			g=0.0;
+			b=0.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
 		//orange
 		case 'f':
-			glColor4f(1.0,0.5,0.0,transparency);	
+			r=1.0;
+			g=0.5;
+			b=0.0;
+			glColor4f(r,g,b,transparency);	
 			colour = 1;
 			break;
 		
 		//yellow
 		case 'g':
-			glColor4f(1.0,1.0,0.0,transparency);	
+			r=1.0;
+			g=1.0;
+			b=0.0;
+			glColor4f(r,g,b,transparency);	
 			colour = 1;
 			break;
 			
 		//green
 		case 'h':
-			glColor4f(0.0,1.0,0.0,transparency);
+			r=0.0;
+			g=1.0;
+			b=0.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;			
 			break;
 		
 		//blue
 		case 'i':
-			glColor4f(0.0,0.0,1.0,transparency);
+			r=0.0;
+			g=0.0;
+			b=1.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
 		//cyan
 		case 'j':
-			glColor4f(0.0,1.0,1.0,transparency);
+			r=0.0;
+			g=1.0;
+			b=1.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
 		//magenta
 		case 'k':
-			glColor4f(1.0,0.0,1.0,transparency);
+			r=1.0;
+			g=0.0;
+			b=1.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
 		//grey
 		case 'l':
-			glColor4f(0.5,0.5,0.5,transparency);
+			r=0.5;
+			g=0.5;
+			b=0.5;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 			
 		//brown
 		case 'm':
-			glColor4f(0.5,0.0,0.0,transparency);
+			r=0.5;
+			g=0.0;
+			b=0.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 			
 		//black
 		case 'n':
-			glColor4f(0.0,0.0,0.0,transparency);
+			r=0.0;
+			g=0.0;
+			b=0.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
 		//white
 		case 'o':
-			glColor4f(1.0,1.0,1.0,transparency);
+			r=1.0;
+			g=1.0;
+			b=1.0;
+			glColor4f(r,g,b,transparency);
 			colour = 1;
 			break;
 		
-		//pallette
+		//Pallette Selection
 		case 'y':
 			glColor4f(r,g,b,transparency);
 			colour = 1;
@@ -314,6 +750,85 @@ void menu(int op) {
 	}
 }
 
+
+//brightness submenu
+void brightness(int op) {
+	switch(op) 
+	{	
+		case '0':
+			r = r+ 1.0;
+			g = g+ 1.0;
+			b = b+ 1.0;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '1':
+			r = r+ 0.8;
+			g = g+ 0.8;
+			b = b+ 0.8;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '2':
+			r = r+ 0.6;
+			g = g+ 0.6;
+			b = b+ 0.6;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '3':
+			r = r+ 0.4;
+			g = g+ 0.4;
+			b = b+ 0.4;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '4':
+			r = r+ 0.2;
+			g = g+ 0.2;
+			b = b+ 0.2;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '5':
+			r = r-0.2;
+			g = g-0.2;
+			b = b-0.2;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '6':
+			r = r-0.4;
+			g = g-0.4;
+			b = b-0.4;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '7':
+			r = r- 0.6;
+			g = g- 0.6;
+			b = b- 0.6;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '8':
+			r = r- 0.8;
+			g = g- 0.8;
+			b = b- 0.8;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+		case '9':
+			r = r- 1.0;
+			g = g- 1.0;
+			b = b- 1.0;
+			glColor4f(r,g,b,transparency);
+			colour = 1;
+			break;
+	}
+}
+
+
 void MyReshape(GLsizei w, GLsizei h)
 {
 	glMatrixMode(GL_PROJECTION);
@@ -325,10 +840,10 @@ void MyReshape(GLsizei w, GLsizei h)
 }
 
 int main(int argc, char **argv){
-	
+	//main window
 	glutInit(&argc, argv);
 	glutInitWindowSize(width,height);
-	glutInitWindowPosition(500,200);
+	glutInitWindowPosition(550,100);
 	glutCreateWindow("Andrew O'Neill - 11424222");
 	glEnable (GL_BLEND);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -343,8 +858,15 @@ int main(int argc, char **argv){
 	int shapeSubMenu = glutCreateMenu(menu);
 	glutAddMenuEntry("Point", 'a');
 	glutAddMenuEntry("Line", 'b');
+	glutAddMenuEntry("Triangle", 'd');
 	glutAddMenuEntry("Rectangle", 'c');
-	glutAddMenuEntry("Polygon", 'd');
+	glutAddMenuEntry("Pentagon", 'r');
+	glutAddMenuEntry("Hexagon", 's');
+	glutAddMenuEntry("Heptagon", 't');
+	glutAddMenuEntry("Octagon", 'u');
+	glutAddMenuEntry("Nonagon", 'v');
+	glutAddMenuEntry("Decagon", 'w');
+	
 	
 	//SUBMENU for Colour
 	int colourSubMenu = glutCreateMenu(menu);
@@ -359,11 +881,20 @@ int main(int argc, char **argv){
 	glutAddMenuEntry("Brown", 'm');
 	glutAddMenuEntry("Black", 'n');
 	glutAddMenuEntry("White", 'o');
-	glutAddMenuEntry("pallette", 'y');
+	glutAddMenuEntry("Pallette", 'y'); //i.e. the colour wheel
 	
-	//SUBMENU for ColourPallette
-	int palletteSubMenu = glutCreateMenu(menu);
-	glutAddMenuEntry("Select Colour", 'p');
+	//SUBMENU for brightness
+	int brightnessSubMenu = glutCreateMenu(brightness);
+	glutAddMenuEntry("+1.0", '0');
+	glutAddMenuEntry("+0.8", '1');
+	glutAddMenuEntry("+0.6", '2');
+	glutAddMenuEntry("+0.4", '3');
+	glutAddMenuEntry("+0.2", '4');
+	glutAddMenuEntry("-0.2", '5');
+	glutAddMenuEntry("-0.4", '6');
+	glutAddMenuEntry("-0.6", '7');
+	glutAddMenuEntry("-0.8", '8');
+	glutAddMenuEntry("-1.0", '9');
 	
 	//SUBMENU for Transparency
 	int transSubMenu = glutCreateMenu(menu);
@@ -383,14 +914,14 @@ int main(int argc, char **argv){
 	glutAddMenuEntry("Clear", 'z');
 	glutAddSubMenu("Shape", shapeSubMenu);
 	glutAddSubMenu("Colour", colourSubMenu);
-	glutAddSubMenu("Colour Pallette", palletteSubMenu);
+	glutAddSubMenu("Brightness", brightnessSubMenu);
 	glutAddSubMenu("Transparency", transSubMenu);
 	glutAddMenuEntry("EXIT", 'q');
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 	
 	//Colour Pallete
 	glutInitWindowSize(width,height);
-	glutInitWindowPosition(100,100);
+	glutInitWindowPosition(0,100);
 	glutCreateWindow("Colour Pallette to choose colour");
 	glutDisplayFunc(colourPallette);
 	glutMouseFunc(touchPallette);
